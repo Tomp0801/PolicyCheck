0aa0ef763e37a6b5657605c3dbbad3f8c71b9ce5
Thomas Pfitzinger <thpfitzinger@web.de>
2022-03-13 18:21:00 +0100

    Policy: (de-)serializing works, just not datetime

--- Policy.py
+++ Policy.py
@@ -19,12 +19,30 @@ class Policy:
         self.source = url
         self.hash = ""
         self.datePublished = None
-        self.dateLastChecked = datetime.now()
+        #self.dateLastChecked = datetime.now()
         self.content = ""
         self.plainText = ""
 
+    @staticmethod
+    def fromJsonFile(jsonFile):
+        with open(jsonFile, "r") as f:
+            jsonObj = json.load(f)
+
+        return Policy.fromJson(jsonObj)
+
+    @staticmethod
+    def fromJson(jsonObj):
+        policyObj = Policy(jsonObj["name"], jsonObj["source"], jsonObj["company"], jsonObj["type"])
+        policyObj.hash = jsonObj["hash"]
+        policyObj.datePublished = jsonObj["datePublished"]          # to type conversion
+        policyObj.content = jsonObj["content"]
+        policyObj.plainText = jsonObj["plainText"]
+
+        return policyObj
+        
+
     def update(self):
-        self.dateLastChecked = datetime.now()
+        #self.dateLastChecked = datetime.now()
         newPolicy = self.fetchPolicy()
         newHash = self.__getHash(newPolicy)
         if newHash != self.hash:
@@ -46,20 +64,25 @@ class Policy:
             return r.text
 
     def HtmlToPlain(self):
-        self.plainText = re.sub("<.*>", "", self.content)
+        # TODO reduce to only interesting div
+        temp = re.sub("<.*>", "", self.content)     # remove html tags
+        self.plainText = re.sub("\s\s+", "", temp)  # remove 2+ whitespaces
+
+    def toJsonFile(self, jsonFile):
+        with open(jsonFile, "w") as f:
+            f.write(self.toJson())
 
     def toJson(self):
-        return json.dumps(self, default=lambda o: str(o), 
-            sort_keys=True, indent=4)
+        return json.dumps(self, default=lambda o: o.__dict__, 
+                            sort_keys=False, indent=4)
 
     @staticmethod
     def __getHash(text):
         return hashlib.md5(text.encode('utf-8')).hexdigest()
 
 
-p = Policy("Reddit User Agreement", "https://www.redditinc.com/policies/user-agreement")
+#p = Policy("Reddit User Agreement", "https://www.redditinc.com/policies/user-agreement")
+
+p = Policy.fromJsonFile("test.json")
 
 p.update()
-
-with  open("test.json", "w") as f:
-    f.write(p.toJson())
