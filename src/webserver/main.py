5a4815acbafd85cf697ae0fede5e8ebc3b3a8921
Thomas Pfitzinger <thpfitzinger@web.de>
2023-07-22 13:33:46 +0200

    prepare files over web interface

--- src/webserver/main.py
+++ src/webserver/main.py
@@ -1,7 +1,10 @@
-from flask import Flask, render_template, send_file, request
+from flask import Flask, render_template, send_file, request, render_template_string
 import signal 
 import sys
 import os 
+sys.path.append("src/scraping")
+from adapters import RedditAdapter, GoogleAdapter
+
 
 app = Flask(__name__)
 
@@ -9,7 +12,8 @@ examples_path = "examples/html"
 
 navBar = [
 	{'name':'Home', 'href':'/'},
-	{'name':'Policies', 'href':'/policies'}
+	{'name':'Policies', 'href':'/policies'},
+    {'name':'Diff', 'href':'/diff'}
 ]
 
 policyLinks = [
@@ -32,6 +36,25 @@ def policies():
 def example():
     args = request.args
     return send_file(os.path.join(examples_path, args.get("folder"), args.get("file")))
+
+@app.route('/prepare')
+def prepare():
+    file = request.args.get("file")
+    type = request.args.get("type")
+    if type and file:
+        file = os.path.join(examples_path, file)
+        save_file = "tmp.html"
+        if type=="reddit":
+            adap = RedditAdapter(file)
+        elif type=="google":
+            adap = GoogleAdapter(file)
+        adap.save(save_file)
+        return render_template_string(adap._soup.prettify())
+
+@app.route('/diff')
+def diff():
+
+    return render_template('diff.html', title="Diff", navBar=navBar)
 
 def signal_handler(sig, frame):
     sys.exit(0)
